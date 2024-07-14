8ae34ed81e81420787859aa42f01f0a9f5325ee5
khoabt94 <khoatienbui94@gmail.com>
2024-07-14 16:17:04 +0700

    feat: add motion to play button

--- client/src/components/common/header/index.tsx
+++ client/src/components/common/header/index.tsx
@@ -3,29 +3,47 @@
 import { Joystick } from 'lucide-react';
 import { Suspense, useMemo } from 'react';
 import { siteConfig } from '@/configs/site';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import UserControl from '../user-control';
+import { motion } from 'framer-motion'
+import './header.styles.css'
 
 const pathToHide = [
   siteConfig.paths.findMatch(),
   siteConfig.paths.matchParent(),
 ]
 
+
 export default function Header() {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
   const shouldHideJoinMatch = useMemo(() => {
     return pathToHide.some(path => pathname.includes(path))
   }, [pathname])
 
   return (
     <div className='h-20 max-w-7xl flex items-center justify-between py-5 px-4 overflow-hidden mx-auto'>
-      {shouldHideJoinMatch ? null : (
-        <Link to={siteConfig.paths.findMatch()}>
-          <Joystick size={40} strokeWidth={1.6} />
+      {shouldHideJoinMatch ? (
+        <Link to={siteConfig.paths.home()} className="">
+          <img src='/logo.webp' alt="Logo" width={180} height={180} />
         </Link>
+      ) : (
+        <motion.button
+          onClick={() => navigate(siteConfig.paths.findMatch())}
+          className='text-lg font-semibold glow-on-hover'
+          whileHover={{
+            scale: 1.05
+          }}
+          whileTap={{
+            scale: 0.95,
+            rotate: '2.5deg'
+          }}
+        >
+          PLAY
+        </motion.button>
       )}
 
-      <div className="">
+      <div className="hidden md:block">
         <img src='/logo.webp' alt="Logo" width={180} height={180} />
       </div>
       <Suspense>
--- client/src/components/common/user-control/index.tsx
+++ client/src/components/common/user-control/index.tsx
@@ -3,7 +3,7 @@ import UserProfile from "./components/user-profile";
 
 export default function UserControl() {
     return (
-        <div className="flex items-center gap-x-5">
+        <div className="flex items-center gap-x-5 ml-auto">
             <UserFriendList />
             <UserProfile />
         </div>
